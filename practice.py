6f12bfbe8f3503d5ef67d8f270f9c1e0429eaaf0
Antar Karmakar <akarmakar@mun.ca>
2025-03-26 13:47:03 -0230

    Printing square pattern 1

--- practice.py
+++ practice.py
@@ -1,1 +1,12 @@
-#print("Hello World")
+#Printing Pattern 1
+
+def pattern1(number):
+    for i in range(0,number,1):
+        for j in range(0,number,1):
+            print("*",end=" ")
+        print()
+
+
+
+number=int(input("Enter the number of lines: "))
+pattern1(number)
