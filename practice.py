f9dadb53432c2188d91b9b51551ef33917d14a47
Antar Karmakar <akarmakar@mun.ca>
2025-03-26 13:54:28 -0230

    updated pattern

--- practice.py
+++ practice.py
@@ -2,9 +2,10 @@
 
 def pattern1(number):
     for i in range(0,number,1):
-        for j in range(0,number,1):
-            print("*",end=" ")
+        for j in range(1,number+1,1):
+            print(f"{j}",end=" ")
         print()
+
 
 
 
