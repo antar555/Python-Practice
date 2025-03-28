76f7b941c3e01fed64346accecccb03c1d342696
Antar Karmakar <akarmakar@mun.ca>
2025-03-28 15:19:54 -0230

    practicing while loop

---
+++ bro_code/whileLoop.py
@@ -0,0 +1,29 @@
+# name=input("Enter your name: ")
+
+# if name=="":
+#     print("You did not enter your name: ")
+# else:
+#     print(f"Hello {name}")
+
+# while name=="":
+#     print("You did not enter your name: ")
+#     name=input("Enter your name: ")
+# print(f"Hello {name}")
+
+# while name=="":
+#     print("You did not enter your name: ")
+#     name=input("Enter your name: ")
+# print(f"Hello {name}")
+
+# while name=="":
+#     print("You did not enter your name: ")
+#     name=input("Enter your name: ")
+# print(name)
+
+# age=int(input("Enter your age: "))
+#
+# while age<0:
+#     print("You did not enter the valid age: ")
+#     age=int(input("Enter your age: "))
+# print(f"your age is: {age}")
+
