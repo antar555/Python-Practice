7b518f7d9197bff0c0bd7a790c4c5420395a6258
Antar Karmakar <akarmakar@mun.ca>
2025-03-29 00:12:15 -0230

    upddated sum until zero

--- self_practice/whileLoop.py
+++ self_practice/whileLoop.py
@@ -1,14 +1,25 @@
 #Sum Until Zero
 #reading numbers until the user enters 0. Output the sum of all entered numbers.
 
-def sumUntilzero():
+# def sumUntilzero():
+#
+#     number=int(input("Enter the number: "))
+#     sum=0
+#     while not number==0:
+#         sum=sum+number
+#         number=int(input("Enter the number: "))
+#     print(f"Sum = {sum}")
+#
+#
+# sumUntilzero()
 
-    number=int(input("Enter the number: "))
+def sumUntilzero(number):
     sum=0
     while not number==0:
         sum=sum+number
         number=int(input("Enter the number: "))
-    print(f"Sum = {sum}")
+    return sum
 
-
-sumUntilzero()
+number=int(input("Enter the number: "))
+result=sumUntilzero(number)
+print(f"Sum = {result}")
