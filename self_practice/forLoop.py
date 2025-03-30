3f2e4ef4acb962610ac3c7122de08fa2be28d83a
Antar Karmakar <akarmakar@mun.ca>
2025-03-30 18:26:53 -0230

    Pattern 2

--- self_practice/forLoop.py
+++ self_practice/forLoop.py
@@ -8,3 +8,14 @@ def pattern1(number):
 
 number=int(input("Enter the number of line: "))
 pattern1(number)
+
+#Printing Pattern 2
+
+def pattern2(number):
+    for i in range(0,number,1):
+        for j in range(1,number+1,1):
+            print(j, end=" ")
+        print()
+
+number=int(input("Enter the number: "))
+pattern2(number)
