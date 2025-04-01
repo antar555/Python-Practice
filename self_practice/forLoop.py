a665665c3495321464ddf921e2f81fe49ef9826a
Antar Karmakar <akarmakar@mun.ca>
2025-04-01 20:31:00 -0230

    pattern 4

--- self_practice/forLoop.py
+++ self_practice/forLoop.py
@@ -1,32 +1,43 @@
-#Printing Pattern 1
+# #Printing Pattern 1
+#
+# def pattern1(number):
+#     for i in range(0, number,1):
+#         for j in range(0,number,1):
+#             print("*", end=" ")
+#         print()
+#
+# number=int(input("Enter the number of line: "))
+# pattern1(number)
+#
+# #Printing Pattern 2
+#
+# def pattern2(number):
+#     for i in range(0,number,1):
+#         for j in range(1,number+1,1):
+#             print(j, end=" ")
+#         print()
+#
+# number=int(input("Enter the number: "))
+# pattern2(number)
+#
+# #pattern 3
+#
+# def pattern3(number):
+#     for i in range(0,number+1,1):
+#         for j in range(1,i+1,1):
+#             print("*", end=" ")
+#         print()
+#
+# number=int(input("Enter the number: "))
+# pattern3(number)
 
-def pattern1(number):
-    for i in range(0, number,1):
-        for j in range(0,number,1):
-            print("*", end=" ")
-        print()
+#pattern 4
 
-number=int(input("Enter the number of line: "))
-pattern1(number)
-
-#Printing Pattern 2
-
-def pattern2(number):
-    for i in range(0,number,1):
-        for j in range(1,number+1,1):
-            print(j, end=" ")
-        print()
-
-number=int(input("Enter the number: "))
-pattern2(number)
-
-#pattern 3
-
-def pattern3(number):
+def pattern4(number):
     for i in range(0,number+1,1):
-        for j in range(1,i+1,1):
+        for j in range(1,number+1-i,1):
             print("*", end=" ")
         print()
 
 number=int(input("Enter the number: "))
-pattern3(number)
+pattern4(number)
