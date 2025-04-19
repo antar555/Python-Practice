8badd7475879ecfa62d6e80bc1425e32557fe1d3
Antar Karmakar <akarmakar@mun.ca>
2025-04-19 00:49:54 -0230

    Accessing elements of a list

--- bro_code/list.py
+++ bro_code/list.py
@@ -1,5 +1,33 @@
-my_list=[5,10,15,20]
-print(my_list[0])
-print(my_list[1])
-print(my_list[2])
-print(my_list[3])
+# my_list=[5,10,15,20]
+# print(my_list[0])
+# print(my_list[1])
+# print(my_list[2])
+# print(my_list[3])
+# print(my_list[-1])
+# print(my_list[-2])
+# print(my_list[-3])
+# print(my_list[-4])
+
+# chat_history=[
+#     "Antar: Hi, how is it going",
+#     "Mahady: Hey, whats up!"
+# ]
+# print(f"The last message is: {chat_history[-1]}")
+
+# my_list=[[1,2,3],"Antar",[4,5,6]]
+# print(my_list[0])
+# print(my_list[0][0])
+# print(my_list[0][1])
+# print(my_list[0][2])
+# print(my_list[1])
+# print(my_list[2])
+# print(my_list[2][0])
+# print(my_list[2][1])
+# print(my_list[2][2])
+
+my_list=[
+    [1,2,3],
+    [["a","b","c"],5,6]
+]
+print(my_list[1][0][1])
+
