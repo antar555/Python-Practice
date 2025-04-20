3ed6fbc1281a051c29c4be0767fe4ff578508bb3
Antar Karmakar <akarmakar@mun.ca>
2025-04-20 21:05:03 -0230

    count positive sum negative in a list

--- self_practice/list.py
+++ self_practice/list.py
@@ -41,6 +41,35 @@ even_numbers=[]
 #     number=number+2
 # print(even_numbers)
 
+def count_positives_count_negatives(arr):
+    if not arr:
+        return []
+    arr_new=[]
+    count_positives=0
+    sum_negatives=0
+    for i in arr:
+        if i>0:
+            count_positives+=1
+
+        elif i<0:
+            sum_negatives+=i
+
+
+
+    arr_new.append(count_positives)
+    arr_new.append(sum_negatives)
+    return arr_new
+
+
+#arr=[1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15]
+arr=[0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]
+print(count_positives_count_negatives(arr))
+
+
+
+
+
+
 
 
 
