195c2d5023a6f0ffe4791b802fbd05e5217df876
Antar Karmakar <akarmakar@mun.ca>
2025-04-21 16:38:22 -0230

    print all the elements using loop

--- self_practice/list.py
+++ self_practice/list.py
@@ -41,29 +41,36 @@ even_numbers=[]
 #     number=number+2
 # print(even_numbers)
 
-def count_positives_count_negatives(arr):
-    if not arr:
-        return []
-    arr_new=[]
-    count_positives=0
-    sum_negatives=0
-    for i in arr:
-        if i>0:
-            count_positives+=1
+# def count_positives_count_negatives(arr):
+#     if not arr:
+#         return []
+#     arr_new=[]
+#     count_positives=0
+#     sum_negatives=0
+#     for i in arr:
+#         if i>0:
+#             count_positives+=1
+#
+#         elif i<0:
+#             sum_negatives+=i
+#
+#
+#
+#     arr_new.append(count_positives)
+#     arr_new.append(sum_negatives)
+#     return arr_new
+#
+#
+# #arr=[1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15]
+# arr=[0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]
+# print(count_positives_count_negatives(arr))
 
-        elif i<0:
-            sum_negatives+=i
+def numbers(elements):
+    for i in elements:
+        print(i)
 
-
-
-    arr_new.append(count_positives)
-    arr_new.append(sum_negatives)
-    return arr_new
-
-
-#arr=[1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15]
-arr=[0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]
-print(count_positives_count_negatives(arr))
+elements=list(map(int, input("Enter the elements: ").split()))
+numbers(elements)
 
 
 
